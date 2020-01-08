fe75b57d4551417339f1d7b28ab17d0b9f3d8277
fengming <1144753090@qq.com>
2020-01-08 17:11:33 +0800

    项目初始化-样式处理

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
     <van-button type="danger">危险按钮</van-button>
     <van-cell title="单元格" value="内容" size="large" />
     <van-cell title="单元格" value="内容" size="large" label="描述信息" />
+   <h2>我们都一样</h2>
   </div>
 </template>
 
@@ -18,8 +19,14 @@ export default {
 </script>
 
 <style lang='less' scoped>
+//在style中使用@别名 需要在前面加上 ~
+// @之后必须有 /
+@import url('~@/styles/variables.less');
   #app{
     margin-top: 50px auto;
 
   }
+h2{
+  color: @color-primary;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import 'normalize.css' // 引入初始化样式 其实vant内置normalize.css了可以不用引入
 import './utils/register-vant'// 加载注册 vant 组件模块
-
+import './styles/index.less' // 注意一定要把自己的样式引到第三方组件样式之后
 Vue.config.productionTip = false
 
 new Vue({
