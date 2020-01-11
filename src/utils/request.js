30ea9cf7485446497eb5b2e92a25ff706829038d
fengming <1144753090@qq.com>
2020-01-11 17:30:33 +0800

    Token处理-发送Token

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 import jsonBig from 'json-bigint'
+// 在非组件模块中直接加载获取容器
+// 这里拿到的 store 和你在组件中访问 this.$store 是一个东西
+import store from '@/store'
 
 // axios.create 方法：复制一个 axios
 const request = axios.create({
@@ -21,29 +24,39 @@ request.defaults.transformResponse = [function (data) {
 }]
 
 // 请求拦截器
-// request.interceptors.request.use(
-//   function (config) {
-//     // Do something before request is sent
-//     return config
-//   },
-//   function (error) {
-//     // Do something with request error
-//     return Promise.reject(error)
-//   }
-// )
+request.interceptors.request.use(
+  function (config) {
+    const { user } = store.state
+
+    // 统一添加 token
+    if (user) {
+      // config.headers 获取操作请求头对象
+      // Authorization 是后端要求的字段名称
+      // 数据值后端要求提供：Bearer token数据
+      // 注意：Bearer 后面有个空格
+      // 老师，为啥？后端要求的
+      config.headers.Authorization = `Bearer ${user.token}`
+    }
+    return config
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error)
+  }
+)
 
 // 响应拦截器
-// request.interceptors.response.use(
-//   function (response) {
-//     // Any status code that lie within the range of 2xx cause this function to trigger
-//     // Do something with response data
-//     return response
-//   },
-//   function (error) {
-//     // Any status codes that falls outside the range of 2xx cause this function to trigger
-//     // Do something with response error
-//     return Promise.reject(error)
-//   }
-// )
+request.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error)
+  }
+)
 
 export default request
