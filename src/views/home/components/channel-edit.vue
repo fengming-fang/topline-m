a5ba2bc3c1a8c32bc7ef8766023a7770c29d242b
fengming <1144753090@qq.com>
2020-01-14 01:19:51 +0800

    频道编辑-展示剩余频道

--- src/views/home/components/channel-edit.vue
+++ src/views/home/components/channel-edit.vue
@@ -18,11 +18,11 @@
 
     <van-cell title="推荐频道" class="channel-header">
     </van-cell>
-    <van-grid :gutter="8" :border="false" clickable>
+    <van-grid :gutter="10" clickable>
     <van-grid-item
-    v-for="value in 14"
-    :key="value"
-    text="文字"
+      v-for="channel in remainingChannels"
+      :key="channel.id"
+      :text="channel.name"
     >
     </van-grid-item>
     </van-grid>
@@ -45,7 +45,22 @@ export default {
       allChannels: [] // 所有频道
     }
   },
-  computed: {},
+  computed: {
+    remainingChannels () {
+      const channels = []
+      const { allChannels, userChannels } = this
+      allChannels.forEach(item => {
+        // 当前的遍历项是否属于我的频道，如果不是，那就收集到 channels
+        // userChannels 是否包含 item
+        // find 会遍历数组，它会对每个元素执行 c.id === item.id 条件判定
+        // 如果 true，则返回该元素，如果直到遍历结束都没有符合条件的元素，则返回 undefined
+        if (!userChannels.find(c => c.id === item.id)) {
+          channels.push(item)
+        }
+      })
+      return channels
+    }
+  },
   watch: {},
   created () {
     this.loadAllChannels()
